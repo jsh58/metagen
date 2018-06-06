284d817910f17e2222c84056f0b26fb685c958f6
john <jsh58@wildcats.unh.edu>
2018-06-06 10:11:20 -0400

    indexNTdb2.sh: include ntSumm.py

--- indexNTdb2.sh
+++ indexNTdb2.sh
@@ -8,14 +8,14 @@
 
 module load centrifuge
 
-# append adapters to nt
-cat adapters2.fa >> nt.fa
+# append adapters to nt db
+db=nt.fa
+cat adapters2.fa >> $db
 cat nodes2.dmp >> nodes.dmp
 cat names2.dmp >> names.dmp
 cat acc2taxid2.txt >> acc2taxid.txt
 
 # index database
-db=nt.fa
 pre=nt
 rm -f $pre.1.cf $pre.2.cf $pre.3.cf $pre.4.cf $pre.tree
 centrifuge-build \
@@ -31,4 +31,12 @@ centrifuge-build \
 centrifuge-inspect \
   --taxonomy-tree \
   $pre \
-  > $pre.tree
+  > $pre.tree.tmp
+
+# add taxonomic summary to tree
+python ntSumm.py \
+  acc2taxid.txt \
+  $pre.tree.tmp \
+  $db \
+  $pre.tree
+rm $pre.tree.tmp
